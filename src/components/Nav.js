4c11c866b733914b2ac3fe9ea4ef4630f4aca24e
Jay-R Doria <jayrdoria876@gmail.com>
2023-07-15 09:18:25 +0800

    Week 2

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+const logo = require('../assets/little-lemon.png');
+
+function Nav() {
+    return (
+      <>
+        <nav>
+          <img src={logo}/>
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Login</a></li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
+
+export default Nav;
